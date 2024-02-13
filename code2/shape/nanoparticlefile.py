62f0299a5b6df9fa14801cb8c15314a22ddca73d
Federico Williamson - WGPC5 <federicowilliamson@hotmail.co.uk>
2024-02-13 13:29:22 -0300

    Add snippet extraction

--- code2/shape/nanoparticlefile.py
+++ code2/shape/nanoparticlefile.py
@@ -1,6 +1,7 @@
 import re
 from dataclasses import dataclass
 from enum import StrEnum
+from functools import cached_property
 from pathlib import Path
 
 DistributionType = StrEnum("DistributionType", ['RANDOM', 'SANDWICH', 'MULTILAYER', 'ONION'])
@@ -18,7 +19,7 @@ class Distribution:
     params: list[str]
 
     @staticmethod
-    def parse(param: str) -> 'Distribution':
+    def of(param: str) -> 'Distribution':
         parts = param.split(".")
         typ: str = parts[0]
         if len(parts) == 1:
@@ -26,7 +27,7 @@ class Distribution:
         try:
             count: int = int(parts[1])
         except ValueError:
-            match = re.match(r"(\d+)\[(.*)\]", parts[1])
+            match = re.match(r"(\d+)\[(.*)]", parts[1])
             assert match is not None, f"Invalid distribution: {param}, {parts[1]}"
             count = int(match.group(1))
             params = [match.group(2), *parts[2:]]
@@ -40,7 +41,7 @@ class Shape:
     type: ShapeType
 
     @staticmethod
-    def parse(param: str) -> 'Shape':
+    def of(param: str) -> 'Shape':
         try:
             upper: str = param.upper()
             return Shape(ShapeType[upper])
@@ -53,7 +54,7 @@ class Interface:
     mix_level: float
 
     @staticmethod
-    def parse(param: str) -> 'Interface':
+    def of(param: str) -> 'Interface':
         if param == "Normal":
             return Interface(0.0)
         elif param.startswith("Mix"):
@@ -66,12 +67,39 @@ class Interface:
 
 
 @dataclass
+class Pores:
+    count: int
+    size: float | None
+
+    @staticmethod
+    def of(param: str) -> 'Pores':
+        if param == "Full":
+            return Pores(0, None)
+        elif param.startswith("Pores."):
+            match = re.match(r"(\d+)(\[(.*)])?", param.split(".")[1])
+            assert match is not None, f"Invalid pores: {param}"
+            count = int(match.group(1))
+            size = float(match.group(3)) if match.group(3) else None
+            return Pores(count, size)
+        else:
+            raise ShapeNameError(f"Invalid pores: {param}")
+
+
+@dataclass
 class NanoparticleData:
     shape: Shape
     distribution: Distribution
     interface: Interface
+    pores: Pores
     index: int
     random_seed: int | None
+
+
+def get_line_index_startswith(lines: list[str], prefix: str) -> int:
+    for index, line in enumerate(lines):
+        if line.startswith(prefix):
+            return index
+    raise AssertionError(f"Line starting with {prefix} not found")
 
 
 @dataclass
@@ -83,15 +111,34 @@ class NanoparticleFile:
         self.path = path
         self.name = path.stem
 
-    def parse(self) -> NanoparticleData:
+    @cached_property
+    def data(self) -> NanoparticleData:
         try:
             parts: list[str] = self.name.split("_")
-            assert len(parts) in (4, 5)
-            shape: Shape = Shape.parse(parts[0])
-            distribution: Distribution = Distribution.parse(parts[1])
-            interface: Interface = Interface.parse(parts[2])
-            index = int(parts[3])
-            random_seed = int(parts[4]) if len(parts) == 5 else None
-            return NanoparticleData(shape, distribution, interface, index, random_seed)
+            assert len(parts) in (5, 6)
+            shape: Shape = Shape.of(parts[0])
+            distribution: Distribution = Distribution.of(parts[1])
+            interface: Interface = Interface.of(parts[2])
+            pores: Pores = Pores.of(parts[3])
+            index = int(parts[4])
+            random_seed = int(parts[5]) if len(parts) == 6 else None
+            return NanoparticleData(shape, distribution, interface, pores, index, random_seed)
         except (ShapeNameError, AssertionError) as e:
             raise ShapeNameError(f"Invalid nanoparticle file name: {self.name}") from e
+
+    @cached_property
+    def content(self):
+        return self.path.read_text()
+
+    @cached_property
+    def lines(self):
+        return self.content.split("\n")
+
+    @cached_property
+    def snippet(self):
+        start = get_line_index_startswith(self.lines, "lattice")
+        try:
+            end = get_line_index_startswith(self.lines, "# setting")
+        except AssertionError:
+            end = get_line_index_startswith(self.lines, "mass")
+        return self.lines[start:end]
--- code2/shape/nanoparticlefile_test.py
+++ code2/shape/nanoparticlefile_test.py
@@ -1,6 +1,14 @@
+import hashlib
 from pathlib import Path
+from typing import TypeVar
 
 import nanoparticlefile as f
+
+T = TypeVar("T")
+
+
+def assert_equal(a: T, b: T, message: str | None = None):
+    assert a == b, message if message is not None else f"Expected {a}, got {b}"
 
 
 def test_distribution():
@@ -12,10 +20,10 @@ def test_distribution():
         "Random": (f.DistributionType.RANDOM, 0, []),
     }
     for name, (typ, count, params) in tests.items():
-        distribution = f.Distribution.parse(name)
-        assert distribution.type == typ
-        assert distribution.count == count
-        assert distribution.params == params
+        distribution: f.Distribution = f.Distribution.of(name)
+        assert_equal(distribution.type, typ)
+        assert_equal(distribution.count, count)
+        assert_equal(distribution.params, params)
 
 
 def test_shape():
@@ -25,7 +33,7 @@ def test_shape():
         "Sphere": f.ShapeType.SPHERE,
     }
     for name, shape in tests.items():
-        assert f.Shape.parse(name).type == shape, f"Expected {shape}, got {f.Shape.parse(name).type}"
+        assert_equal(f.Shape.of(name).type, shape, f"Expected {shape}, got {f.Shape.of(name).type}")
 
 
 def test_interface():
@@ -35,32 +43,45 @@ def test_interface():
         "Mix.10": 10,
     }
     for name, mix_level in tests.items():
-        assert f.Interface.parse(name).mix_level == mix_level
+        assert_equal(f.Interface.of(name).mix_level, mix_level)
 
 
-def test_nanoparticlefile():
+def test_nanoparticle_file_name():
     tests: dict[str, tuple[f.DistributionType, int, list[str], f.ShapeType, float, int, int]] = {
-        "../Shapes/Cone_Random_Normal_0.in": (f.DistributionType.RANDOM, 0, [], f.ShapeType.CONE, 0.0, 0, None),
-        "../Shapes/Cone_Random_Normal_0_123.in": (f.DistributionType.RANDOM, 0, [], f.ShapeType.CONE, 0.0, 0, 123),
-        "../Shapes/Sphere_Onion.2[Cone]_Mix.05_1.in": (f.DistributionType.ONION, 2, ["Cone"], f.ShapeType.SPHERE, 5.0, 1, None),
-        "../Shapes/Cube_Sandwich.2.X_Mix.10_2.in": (f.DistributionType.SANDWICH, 2, ["X"], f.ShapeType.CUBE, 10.0, 2, None),
-        "../Shapes/Octahedron_Random_Normal_0_3.in": (f.DistributionType.RANDOM, 0, [], f.ShapeType.OCTAHEDRON, 0.0, 0, 3),
+        "../Shapes/Cone_Random_Normal_Full_0.in": (f.DistributionType.RANDOM, 0, [], f.ShapeType.CONE, 0.0, 0, None),
+        "../Shapes/Cone_Random_Normal_Pores.1_0_123.in": (f.DistributionType.RANDOM, 0, [], f.ShapeType.CONE, 0.0, 0, 123),
+        "../Shapes/Sphere_Onion.2[Cone]_Mix.05_Pores.2[7.2A]_1.in": (f.DistributionType.ONION, 2, ["Cone"], f.ShapeType.SPHERE, 5.0, 1, None),
+        "../Shapes/Cube_Sandwich.2.X_Mix.10_Full_2.in": (f.DistributionType.SANDWICH, 2, ["X"], f.ShapeType.CUBE, 10.0, 2, None),
+        "../Shapes/Octahedron_Random_Normal_Full_0_3.in": (f.DistributionType.RANDOM, 0, [], f.ShapeType.OCTAHEDRON, 0.0, 0, 3),
     }
     for name, (distribution_type, count, params, shape_type, mix_level, index, random_seed) in tests.items():
-        nanoparticle_file = f.NanoparticleFile(Path(name))
-        nanoparticle_data = nanoparticle_file.parse()
-        assert nanoparticle_data.shape.type == shape_type, f"Expected {shape_type}, got {nanoparticle_data.shape.type}"
-        assert nanoparticle_data.distribution.type == distribution_type, f"Expected {distribution_type}, got {nanoparticle_data.distribution.type}"
-        assert nanoparticle_data.distribution.count == count, f"Expected {count}, got {nanoparticle_data.distribution.count}"
-        assert nanoparticle_data.distribution.params == params, f"Expected {params}, got {nanoparticle_data.distribution.params}"
-        assert nanoparticle_data.interface.mix_level == mix_level, f"Expected {mix_level}, got {nanoparticle_data.interface.mix_level}"
-        assert nanoparticle_data.index == index, f"Expected {index}, got {nanoparticle_data.index}"
-        assert nanoparticle_data.random_seed == random_seed, f"Expected {random_seed}, got {nanoparticle_data.random_seed}"
+        nanoparticle_file: f.NanoparticleFile = f.NanoparticleFile(Path(name))
+        nanoparticle_data: f.NanoparticleData = nanoparticle_file.data
+        assert_equal(nanoparticle_data.shape.type, shape_type)
+        assert_equal(nanoparticle_data.distribution.type, distribution_type)
+        assert_equal(nanoparticle_data.distribution.count, count)
+        assert_equal(nanoparticle_data.distribution.params, params)
+        assert_equal(nanoparticle_data.interface.mix_level, mix_level)
+        assert_equal(nanoparticle_data.index, index)
+        assert_equal(nanoparticle_data.random_seed, random_seed)
+
+
+def test_nanoparticle_snippet():
+    files: dict[str, str] = {
+        "../Shapes/Cone_Random_Normal_Full_0.in": "77a20abbfa50625df0f2257a2d9644a1",
+        "../Shapes/Cone_Random_Normal_Pores.2_0.in": "7e25e6bd57fedaedaf12b70678c7ce1c",
+    }
+    for name, index in files.items():
+        nanoparticle_file: f.NanoparticleFile = f.NanoparticleFile(Path(name))
+        snippet: str = "\n".join(nanoparticle_file.snippet)
+        snippet_hash: str = hashlib.md5(snippet.encode()).hexdigest()
+        assert_equal(snippet_hash, index, f"Expected {index}, got {snippet_hash}")
 
 
 if __name__ == "__main__":
     test_distribution()
     test_shape()
     test_interface()
-    test_nanoparticlefile()
+    test_nanoparticle_file_name()
+    test_nanoparticle_snippet()
     print("nanoparticlefile_test.py: all tests pass")
